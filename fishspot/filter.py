a1c2277998ea7f9faf1919ee2db379f0cff245bb
gfleishman <greg.nli10me@gmail.com>
2021-02-15 11:06:57 -0500

    reorganized

--- fishspot/filter.py
+++ fishspot/filter.py
@@ -1,5 +1,15 @@
+import numpy as np
+from skimage.morphology import white_tophat as skimage_white_tophat
 from skimage.exposure import rescale_intensity
 from skimage.restoration import richardson_lucy
+
+
+def white_tophat(image, radius):
+    """
+    """
+
+    selem = np.ones((radius,)*image.ndim)
+    return skimage_white_tophat(image, selem=selem)
 
 
 def rl_decon(image, psf, **kwargs):
@@ -12,5 +22,4 @@ def rl_decon(image, psf, **kwargs):
         out_range=(0, 1),
     )
     return richardson_lucy(norm_image, psf, **kwargs)
-
 
