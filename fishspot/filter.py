61ea96d75f19c514179b6ce2d09981a6e518140b
Sebastian Quiroz <81994107+SebastianQuiroz7422@users.noreply.github.com>
2022-02-08 19:06:41 +0100

    Update filter.py
    Added the wiener convolution in 3D

--- fishspot/filter.py
+++ fishspot/filter.py
@@ -26,3 +26,20 @@ def rl_decon(image, psf, **kwargs):
     # run decon
     return richardson_lucy(norm_image, psf, **kwargs)
 
+def wiener_decon(image, psf, nsr=1e-3):
+    """
+    """
+
+    # normalize image
+    norm_image = rescale_intensity(
+        image,
+        in_range=(0, image.max()),
+        out_range=(0, 1),
+    )
+
+    # run decon
+    G = np.fft.fftn(norm_image)
+    H = np.fft.fftn(psf, s=norm_image.shape)
+    H = np.conj(H) / (np.abs(H)**2 + nsr)
+
+    return np.abs(np.fft.ifftn(G * H))
